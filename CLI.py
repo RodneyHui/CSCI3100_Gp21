e5fdf95acd2f5dc31e8a0af9caf1cff6031bd230
rolandtsang <rolandtsang@gmail.com>
2025-12-10 22:13:46 +0800

    Add Move Task, error catches for type error, and fix bugs regarding leaving empty fields for Status and DueDate

--- CLI.py
+++ CLI.py
@@ -42,17 +42,21 @@ def interactive_menu(store: str):
         elif choice == "2":
             #Add task
             Title = input("Title: ").strip()
-            StatusInput = int(input("Status (1: To-Do 2: In Progress 3: Waiting Review 4: Finished): ").strip())
-            if StatusInput == 1:
-                Status = "To-Do"
-            elif StatusInput == 2:
-                Status = "In Progress"
-            elif StatusInput == 3:
-                Status = "Waiting Review"
-            elif StatusInput == 4:
-                Status = "Finished"
-            else:
-                print("Status is not valid")
+            try:
+                StatusInput = int(input("Status (1: To-Do 2: In Progress 3: Waiting Review 4: Finished): ").strip())
+                if StatusInput == 1:
+                    Status = "To-Do"
+                elif StatusInput == 2:
+                    Status = "In Progress"
+                elif StatusInput == 3:
+                    Status = "Waiting Review"
+                elif StatusInput == 4:
+                    Status = "Finished"
+                else:
+                    print("Status is not valid")
+                    continue
+            except(ValueError):
+                print("Please enter a valid number.")
                 continue
             PersonInCharge = input("Person in charge: ").strip()
             DueDateInput = input("Due date (YYYY-MM-DD): ").strip()
@@ -71,37 +75,74 @@ def interactive_menu(store: str):
 
         elif choice == "3":
             #Move task
-
-            return
+            try:
+                TaskID = int(input("Task ID: ").strip()) - 1
+            except(ValueError):
+                print("Please enter a valid number.")
+                continue
+            Editor = input("Name of Editor: ").strip()
+            StatusInput = (input("New status (1: To-Do 2: In Progress 3: Waiting Review 4: Finished Blank: Cancel): ").strip()) or None
+            if StatusInput == None:
+                Status = None
+            else:
+                try:
+                    StatusInput = int(StatusInput)
+                    if StatusInput == 1:
+                        Status = "To-Do"
+                    elif StatusInput == 2:
+                        Status = "In Progress"
+                    elif StatusInput == 3:
+                        Status = "Waiting Review"
+                    elif StatusInput == 4:
+                        Status = "Finished"
+                    else:
+                        print("Status is not valid")
+                        continue
+                except(ValueError):
+                    print("Please enter a valid number.")
+                    continue
+            board.EditTask(TaskID, Editor, NewStatus=Status)
 
         elif choice == "4":
             #Edit task
+
             TaskID = int(input("Task ID: ").strip()) - 1
             Editor = input("Name of Editor: ").strip()
             Title = input("New title (blank to skip): ").strip() or None
-            StatusInput = int(input("New status (1: To-Do 2: In Progress 3: Waiting Review 4: Finished Blank: Skip): ").strip()) or None
-            if StatusInput == 1:
-                Status = "To-Do"
-            elif StatusInput == 2:
-                Status = "In Progress"
-            elif StatusInput == 3:
-                Status = "Waiting Review"
-            elif StatusInput == 4:
-                Status = "Finished"
+            StatusInput = (input("New status (1: To-Do 2: In Progress 3: Waiting Review 4: Finished Blank: Skip): ").strip()) or None
+            if StatusInput == None:
+                Status = None
             else:
-                print("Status is not valid")
-                continue
+                try:
+                    StatusInput = int(StatusInput)
+                    if StatusInput == 1:
+                        Status = "To-Do"
+                    elif StatusInput == 2:
+                        Status = "In Progress"
+                    elif StatusInput == 3:
+                        Status = "Waiting Review"
+                    elif StatusInput == 4:
+                        Status = "Finished"
+                    else:
+                        print("Status is not valid")
+                        continue
+                except(ValueError):
+                    print("Please enter a valid number.")
+                    continue
             PersonInCharge = input("New person in charge (blank to skip): ").strip() or None
-            DueDateInput = input("New due date (YYYY-MM-DD) (blank to skip): ").strip()
-            parts = DueDateInput.split("-")
-            if (len(parts) == 3 and
-                parts[0].isdigit() and len(parts[0]) == 4 and  # Year
-                parts[1].isdigit() and len(parts[1]) == 2 and  # Month
-                parts[2].isdigit() and len(parts[2]) == 2):    # Day
-                DueDate = DueDateInput
+            DueDateInput = input("New due date (YYYY-MM-DD) (blak to skip): ").strip() or None
+            if DueDateInput == None:
+                DueDate = None
             else:
-                print("Invalid date format.")
-                continue
+                parts = DueDateInput.split("-")
+                if (len(parts) == 3 and
+                    parts[0].isdigit() and len(parts[0]) == 4 and  # Year
+                    parts[1].isdigit() and len(parts[1]) == 2 and  # Month
+                    parts[2].isdigit() and len(parts[2]) == 2):    # Day
+                    DueDate = DueDateInput
+                else:
+                    print("Invalid date format.")
+                    continue
             AdditionalInfo = input("New additional information (blank to skip): ").strip() or None
             
             #Increment TaskID to be more consistent to the display (ID displayed in List tasks: 1, actual internal ID: 0)
@@ -109,7 +150,11 @@ def interactive_menu(store: str):
 
         elif choice == "5":
             #Delete task
-            TaskID = int(input("Task ID: ").strip()) - 1
+            try:
+              TaskID = int(input("Task ID: ").strip()) - 1
+            except(ValueError):
+                print("Please enter a valid number")
+                continue
             Confirm = input(f"Confirm remove {TaskID}? (y/N): ").strip().lower()
             if Confirm == "y":
                 board.DelTask(TaskID)
@@ -117,8 +162,15 @@ def interactive_menu(store: str):
 
         elif choice == "6":
             #Show task
-            TaskID = int(input("Task ID: ").strip()) - 1
-            board.tasks[TaskID].DisplayTask()
+            try:
+                TaskID = int(input("Task ID: ").strip()) - 1
+            except(ValueError):
+                print("Please enter a valid number")
+                continue
+            try:
+                board.tasks[TaskID].DisplayTask()
+            except(IndexError):
+                print("Task not found, 6.")
         
         elif choice == "7":
             #Advise
@@ -131,7 +183,7 @@ def interactive_menu(store: str):
             print("Invalid choice. Please enter a number from the menu.")
 
 
-"""
+
 def main(argv=None):
         try:
             interactive_menu("~/.kanban/board.json")
@@ -141,4 +193,4 @@ def main(argv=None):
 
 if __name__ == "__main__":
     main()
-"""
+
